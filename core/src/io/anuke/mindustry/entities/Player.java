2c3f0ab73caba888b64c9c94f6c30d7162685051
Gab_351 <35538496+Gab351@users.noreply.github.com>
2018-01-18 07:38:11 +0100

    change player speed

--- core/src/io/anuke/mindustry/entities/Player.java
+++ core/src/io/anuke/mindustry/entities/Player.java
@@ -16,8 +16,8 @@ import java.nio.ByteBuffer;
 import static io.anuke.mindustry.Vars.*;
 
 public class Player extends SyncEntity{
-	static final float speed = 1.1f;
-	static final float dashSpeed = 1.8f;
+	static final float speed = 2f;
+	static final float dashSpeed = 3.5f;
 
 	public String name = "name";
 	public boolean isAndroid;
@@ -38,7 +38,7 @@ public class Player extends SyncEntity{
 		hitbox.setSize(5);
 		hitboxTile.setSize(4f);
 		
-		maxhealth = 20000;
+		maxhealth = 200000;
 		heal();
 	}
 
