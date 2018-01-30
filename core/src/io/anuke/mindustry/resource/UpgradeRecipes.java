0071acb021dcc4217ca5c5583d8b8c63d2553591
Gab_351 <35538496+Gab351@users.noreply.github.com>
2018-01-30 19:10:28 +0100

    Update UpgradeRecipes.java

--- core/src/io/anuke/mindustry/resource/UpgradeRecipes.java
+++ core/src/io/anuke/mindustry/resource/UpgradeRecipes.java
@@ -6,11 +6,11 @@ import io.anuke.ucore.util.Mathf;
 
 public class UpgradeRecipes {
     private static final ObjectMap<Upgrade, ItemStack[]> recipes = Mathf.map(
-            Weapon.triblaster, list(stack(Item.iron, 4), stack(Item.steel, 4)),
-            Weapon.clustergun, list(stack(Item.iron, 6), stack(Item.steel, 8)),
-            Weapon.vulcan, list(stack(Item.iron, 300), stack(Item.steel, 600), stack(Item.titanium, 300)),
-            Weapon.beam, list(stack(Item.steel, 24), stack(Item.titanium, 12), stack(Item.dirium, 8)),
-            Weapon.shockgun, list(stack(Item.steel, 12), stack(Item.titanium, 12), stack(Item.dirium, 12))
+            Weapon.triblaster, list(stack(Item.iron, 20), stack(Item.steel, 20)),
+            Weapon.clustergun, list(stack(Item.iron, 30), stack(Item.steel, 40)),
+            Weapon.vulcan, list(stack(Item.iron, 15), stack(Item.steel, 30), stack(Item.titanium, 15)),
+            Weapon.beam, list(stack(Item.steel, 120), stack(Item.titanium, 60), stack(Item.dirium, 40)),
+            Weapon.shockgun, list(stack(Item.steel, 60), stack(Item.titanium, 60), stack(Item.dirium, 60))
     );
 
     private static final ItemStack[] empty = {};
