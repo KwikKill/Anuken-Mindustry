291121c2a19ab30827c314aa7123435ea1b4ed1a
Gab_351 <35538496+Gab351@users.noreply.github.com>
2018-01-31 18:07:46 +0100

    Create ModBlocks.java

---
+++ core/src/io/anuke/mindustry/world/blocks/ModBlocks.java
@@ -0,0 +1,21 @@
+package io.anuke.mindustry.world.blocks;
+
+import io.anuke.mindustry.graphics.Fx;
+import io.anuke.mindustry.world.blocks.types.Wall;
+import io.anuke.mindustry.world.Block;
+import io.anuke.mindustry.world.blocks.types.distribution.*;
+
+public class ModBlocks{
+	
+	public static final Block
+    static final int wallHealthMultiplier = 4;
+	
+	omniconveyor = new Conveyor("omniconveyor"){{
+		           health = 90;
+	             speed = 0.15f;
+	}},
+  incassablium = new Wall("incassablium"){{
+	   	         health = 1000*25*wallHealthMultiplier;
+               width = height = 5;
+  }},
+}
