22341b9a27019a709be6e14efaada95c9eb3073f
Gab_351 <35538496+Gab351@users.noreply.github.com>
2018-01-31 20:58:51 +0100

    Update ModBlocks.java

--- core/src/io/anuke/mindustry/world/blocks/ModBlocks.java
+++ core/src/io/anuke/mindustry/world/blocks/ModBlocks.java
@@ -7,15 +7,15 @@ import io.anuke.mindustry.world.blocks.types.distribution.*;
 
 public class ModBlocks{
 	
-	public static final Block
-    static final int wallHealthMultiplier = 4;
+        public static final Block
+        static final int wallHealthMultiplier = 4;
 	
 	omniconveyor = new Conveyor("omniconveyor"){{
-		           health = 90;
+		     health = 90;
 	             speed = 0.15f;
 	}},
-  incassablium = new Wall("incassablium"){{
-	   	         health = 1000*25*wallHealthMultiplier;
-               width = height = 5;
-  }},
+        incassablium = new Wall("incassablium"){{
+	   	     health = 1000*25*wallHealthMultiplier;
+                     width = height = 5;
+        }},
 }
