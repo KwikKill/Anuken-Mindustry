1f46c922745b90f100d30dd6b5d1bea216c43534
Gab_351 <35538496+Gab351@users.noreply.github.com>
2018-02-01 20:57:14 +0100

    Update UpragdeMechRecipes.java

--- core/src/io/anuke/mindustry/resource/UpragdeMechRecipes.java
+++ core/src/io/anuke/mindustry/resource/UpragdeMechRecipes.java
@@ -8,7 +8,7 @@ public class UpgradeMechRecipes {
     private static final ObjectMap<Upgrade, ItemStack[]> recipes = Mathf.map(
             Mech.omnimech, list(stack(Item.iron, 40), stack(Item.steel, 40)),
             Mech.speedmech, list(stack(Item.iron, 60), stack(Item.steel, 80)),
-            Mech.defensemech, list(stack(Item.iron, 60), stack(Item.steel, 60), stack(Item.titanium, 30)),
+            Mech.defensemech, list(stack(Item.iron, 60), stack(Item.steel, 60), stack(Item.titanium, 30))
         );
 
     private static final ItemStack[] empty = {};
