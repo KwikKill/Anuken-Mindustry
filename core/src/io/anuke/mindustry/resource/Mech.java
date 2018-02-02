089e372ed39d6fad001704258cdd24835da57693
Gab_351 <35538496+Gab351@users.noreply.github.com>
2018-02-02 07:33:12 +0100

    Update Mech.java

--- core/src/io/anuke/mindustry/resource/Mech.java
+++ core/src/io/anuke/mindustry/resource/Mech.java
@@ -7,18 +7,18 @@ public class Mech extends Upgrade{
 
 	public Mech(String name){
 		super(name);
-	},
+	}
 
 	omnimech = new Mech("omnimech");
 
 	public Mech(String name){
 		super(name);
-	},
+	}
 	speedmech = new Mech("speedmech");
 
 	public Mech(String name){
 		super(name);
-	},
+	}
 	defensemech = new Mech("defensemech");
 
 	public Mech(String name){
