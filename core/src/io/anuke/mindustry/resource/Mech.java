7f1d1d4bb48d3e564313322bb17d549e2b672856
Gab_351 <35538496+Gab351@users.noreply.github.com>
2018-02-02 17:03:43 +0100

    Update Mech.java

--- core/src/io/anuke/mindustry/resource/Mech.java
+++ core/src/io/anuke/mindustry/resource/Mech.java
@@ -2,12 +2,7 @@ package io.anuke.mindustry.resource;
 
 public class Mech extends Upgrade{
 	public static final Mech
-
 	standard = new Mech("standard");
-
-	public Mech(String name){
-		super(name);
-	}
 	
 	public static final Mech
 	omnimech = new Mech("omnimech");
@@ -17,4 +12,9 @@ public class Mech extends Upgrade{
 	
 	public static final Mech
 	defensemech = new Mech("defensemech");
+	
+	public Mech(String name){
+		super(name);
+	}
+	
 }
