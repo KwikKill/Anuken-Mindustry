3305b89fcdea1a27501846c4105154136b75bcf0
Gab_351 <35538496+Gab351@users.noreply.github.com>
2018-02-02 17:39:01 +0100

    Create decration.java

---
+++ core/src/io/anuke/mindustry/world/blocks/types/decration.java
@@ -0,0 +1,15 @@
+package io.anuke.mindustry.world.blocks.types;
+
+import io.anuke.mindustry.world.Block;
+
+public class Wall extends Block{
+
+	public Decoration(String name) {
+		super(name);
+		solid = false;
+		destructible = true;
+	}
+	
+
+
+}
