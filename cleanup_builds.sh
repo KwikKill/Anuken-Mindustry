85c53e22d08ddabc860627b2d757ba90f9e81205
Gab351 <gabriel@blaisot.org>
2018-02-06 21:35:25 +0100

    fix bug

--- cleanup_builds.sh
+++ cleanup_builds.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ls *-Gab351-mod.jar | sort | head -n +7 | cut -d'-' -f 1 | while read oldbuild; do
+ls *-Gab351-mod.jar | sort | head -n -7 | cut -d'-' -f 1 | while read oldbuild; do
   OLD_DESKFILE="${oldbuild}-Gab351-mod.jar"
   OLD_FILE1="Bleeding-Edge-Build-${oldbuild}.md"
   [ -e "${OLD_FILE1}" ] && git rm -f ${OLD_FILE1}
