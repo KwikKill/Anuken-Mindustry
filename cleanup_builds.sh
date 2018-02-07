8811e9d5dc69283a1b463d50e74cfabad515e6b5
Gab351 <gabriel@blaisot.org>
2018-02-07 20:56:23 +0100

    improve cleanup to remove builds with old file naming

--- cleanup_builds.sh
+++ cleanup_builds.sh
@@ -5,12 +5,16 @@ pwd
 ls
 echo "---"
 
-ls *-Gab351-mod.jar | sort | head -n -7 | cut -d'-' -f 1 | while read oldbuild; do
+ls *.jar | cut -d'-' -f 1 | sort -n | head -n -7 |  while read oldbuild; do
   echo "cleaning up ${oldbuild}"
   OLD_DESKFILE="${oldbuild}-Gab351-mod.jar"
+  OLD_OLD_DESKFILE="${oldbuild}-gab_351-mod.jar"
+  OLD_OLD_OLD_DESKFILE="${oldbuild}-desktop-bleeding-edge.jar"
   OLD_FILE1="Bleeding-Edge-Build-${oldbuild}.md"
   [ -e "${OLD_FILE1}" ] && git rm -f ${OLD_FILE1}
   [ -e "${OLD_DESKFILE}" ] && git rm -f ${OLD_DESKFILE}
+  [ -e "${OLD_OLD_DESKFILE}" ] && git rm -f ${OLD_OLD_DESKFILE}
+  [ -e "${OLD_OLD_OLD_DESKFILE}" ] && git rm -f ${OLD_OLD_OLD_DESKFILE}
 done
 
 git status
