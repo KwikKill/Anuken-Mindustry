72bfdbcfa41811643364200649f81e23a19efbaa
Gab351 <gabriel@blaisot.org>
2018-02-09 21:28:38 +0100

    fix minor bug

--- core/src/io/anuke/mindustry/world/blocks/types/Decoration.java
+++ core/src/io/anuke/mindustry/world/blocks/types/Decoration.java
@@ -5,7 +5,6 @@ import io.anuke.mindustry.world.Block;
 public class Decoration extends Block{
 
 	public Decoration(String name) {
-		shadow = name;
 		super(name);
 		solid = false;
 		breakable = true;
