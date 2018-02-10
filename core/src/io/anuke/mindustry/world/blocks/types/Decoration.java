b054934b2b7ba0a7a3a3af84b5daded86e15ae7c
Gab351 <gabriel@blaisot.org>
2018-02-10 15:29:59 +0100

    test to add shadow to decoration blocks

--- core/src/io/anuke/mindustry/world/blocks/types/Decoration.java
+++ core/src/io/anuke/mindustry/world/blocks/types/Decoration.java
@@ -5,6 +5,7 @@ import io.anuke.mindustry.world.Block;
 public class Decoration extends Block{
 
 	public Decoration(String name) {
+		shadow = name+"shadow";
 		super(name);
 		solid = false;
 		breakable = true;
